e8a12c8e0e3a97731e4bb5c883502866e87da249
Amaresh Maharana <amareshcoder@gmail.com>
2025-07-05 06:52:14 +0530

    feat(api): add POST endpoint for creating new users

--- backend-api-project-01/index.js
+++ backend-api-project-01/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import fs from "fs";
+
 import mockData from "./MOCK_DATA.json" assert { type: "json" };
 
 const app = express();
 const PORT = 5000;
+
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
+
 // for only this route - /users - we send a html
+// getting all users
 app.get("/users", (req, res) => {
   const htmlDoc = `
         <ul>
@@ -11,6 +18,11 @@ app.get("/users", (req, res) => {
         </ul>
     `;
   res.send(htmlDoc);
+});
+
+// getting all users with API
+app.get("/api/users", (req, res) => {
+  return res.json(mockData);
 });
 
 // ++++ REST API ++++
@@ -36,12 +48,18 @@ app
     return res.json({ status: "Pending" });
   });
 
-
-
 // using POST Method
-// app.post('/api/users', (req, res) => {
-//     return res.json({status: 'Pending...'})
-// })
+app.post("/api/users", (req, res) => {
+  // create new user
+  const body = req.body;
+  mockData.push({...body, id: mockData.length + 1});
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(mockData), (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    return res.json({ status: "Success", id: mockData.length });
+  });
+});
 
 // using PATCH Method
 // app.patch('/api/users/:id', (req, res) => {
